0bc1e3d23dcaf514b2afa885c541118a0a769535
AsrafuzzamanKhan <asrafuzzamankhan89@gmail.com>
2024-05-10 16:02:22 +0600

    todo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import noteImg from '../assets/images/notes.png';
 import doubleTick from '../assets/images/double-tick.png';
 import plusImg from '../assets/images/plus.png';
+import { useDispatch } from 'react-redux';
+import { added, allCompleted, clearCompleted } from '../redux/todos/actions';
 export default function Header() {
+  const [input, setInput] = useState('');
+  const dispatch = useDispatch()
+
+  const handleInput = e => {
+    setInput(e.target.value)
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(added(input))
+    setInput("")
+  }
+  const completeAll = () => {
+    dispatch(allCompleted())
+  }
+  const deleteAll = () => {
+    dispatch(clearCompleted())
+  }
+
   return (
 
     <div>
-      <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+      <form onSubmit={submitHandler} className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
         <img src={noteImg} className="w-6 h-6" alt="Add todo" />
         <input
           type="text"
           placeholder="Type your todo"
+          value={input}
+          onChange={handleInput}
           className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
         />
         <button
           type="submit"
-          className={`appearance-none w-8 h-8 bg-[url('${plusImg}')] bg-no-repeat bg-contain`}
+          className={`appearance-none w-8 h-8 bg-[url('${plusImg}')] bg-no-repeat bg-contain bg-slate-950`}
         ></button>
       </form>
 
       <ul className="flex justify-between my-4 text-xs text-gray-500">
-        <li className="flex space-x-1 cursor-pointer">
+        <li onClick={completeAll} className="flex space-x-1 cursor-pointer">
           <img className="w-4 h-4" src={doubleTick} alt="Complete" />
           <span>Complete All Tasks</span>
         </li>
-        <li className="cursor-pointer">Clear completed</li>
+        <li onClick={deleteAll} className="cursor-pointer">Clear completed</li>
       </ul>
     </div>
 
